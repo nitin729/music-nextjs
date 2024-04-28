28984a28b27562a5f3327bca6b04c5ef334fec85
Nitin Jadhav <nitin117@tuta.io>
2024-04-28 18:17:39 +0530

    hero section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Hero from "./components/hero";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-3xl text-center">Music Next</h1>
+      <Hero />
     </main>
   );
 }
