1671d06a7ed49e82ccb2c1822b1b282105f11185
Nitin Jadhav <nitin117@tuta.io>
2024-04-28 20:57:18 +0530

    featured courses section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import FeaturedCourses from "./components/FeaturedCourses";
 import Hero from "./components/hero";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Hero />
+      <FeaturedCourses />
     </main>
   );
 }
